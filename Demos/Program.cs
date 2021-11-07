58f83f7aae7186e4c78e691d771b4d45d66760e3
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2021-11-07 19:36:58 +0300

    last update

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -10,6 +10,43 @@ namespace Demos
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Adınız: ");
+            string ad = Console.ReadLine();
+            Console.Write("Yaşınız: ");
+            int yas = Convert.ToInt32
+                (Console.ReadLine());
+            Console.Write("Eğitim Durumu (İlkokul: i, ortaokul: o, lise: l, üniversite: ü): ");
+            string egitim = Console.ReadLine();
+            Console.Write("Renk körü müsün? (Evet: e, Hayır: h)");
+            string renkKoruMu = Console.ReadLine();
+            if (yas >=18)
+            {
+                if (egitim == "l" || egitim == "ü")
+                {
+                    
+                   
+                    if (renkKoruMu == "h")
+                    {
+                        Console.WriteLine("Sayın " + ad + ", ehliyet alabilirsiniz.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sayın " + ad + ", ehliyet alamazsınız. Çünkü Renk körü hastalığınız vardır.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sayın " + ad + ", ehliyet alamazsınız. Çünkü eğitim durumunuz kriterleri karşılamıyor");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sayın " + ad + ", ehliyet alamazsınız. Çünkü yaşınız 18den küçük." );
+                
+            }
+            Console.ReadLine();
+
 
         }
     }
