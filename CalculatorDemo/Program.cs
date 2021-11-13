2a91739b484394a191ad2f9b597eac09409ab8d8
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2021-11-13 19:46:15 +0300

    hesap makinesi

--- CalculatorDemo/Program.cs
+++ CalculatorDemo/Program.cs
@@ -10,6 +10,32 @@ namespace CalculatorDemo
     {
         static void Main(string[] args)
         {
+            Console.Write("1. sayı: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2.Sayı: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("(+, -, *, /): ");
+            string islem = Console.ReadLine();
+            double islemsonucu = IslemYap(sayi1, sayi2, islem);
+            Console.WriteLine("Sonuç: " + islemsonucu);
+            Console.ReadLine();
         }
+
+        static double IslemYap(double sayi1, double sayi2, string islem)
+        {
+            double sonuc;
+            if (islem == "+")
+                sonuc = sayi1 + sayi2;
+            else if (islem == "-")
+                sonuc = sayi1 - sayi2;
+            else if (islem == "*")
+                sonuc = sayi1 * sayi2;
+            else // islem == "/"
+                sonuc = sayi1 / sayi2;
+            return sonuc;
+
+
+        }
+
     }
 }
