4adb7a18ad13fba6032d880df8d83994b4ae424f
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2021-11-28 19:22:43 +0300

    datetime

---
+++ DateTimeDataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime simdi = DateTime.Now;
+            Console.WriteLine(simdi);
+            Console.WriteLine(simdi.ToShortDateString());
+            Console.WriteLine(simdi.ToLongDateString());
+            Console.WriteLine(simdi.ToShortTimeString());
+            Console.WriteLine(simdi.ToLongTimeString());
+            Console.WriteLine(simdi.ToShortDateString() + " " + simdi.ToLongTimeString());
+            Console.WriteLine(simdi.Month + "/" + simdi.Day + "/" + simdi.Year + " " + simdi.Hour + ":" + simdi.Minute + ":" + simdi.Second + "." + simdi.Millisecond);
+
+            DateTime tarih = new DateTime(2021, 11, 28, 19, 17, 0);
+            Console.WriteLine(tarih);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StringsDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+
+            string ornek = "as good as it gets.";
+            string deger = "as";
+            int index = ornek.IndexOf(deger); // 0
+            Console.WriteLine(index);
+            int lastIndex = ornek.LastIndexOf(deger); // 8
+            string tersineOrnek = TersineCevir(ornek);
+            string tersineDeger = TersineCevir(deger);
+            int tersineLastIndex = tersineOrnek.IndexOf(tersineDeger); // 9 
+            Console.WriteLine(tersineLastIndex);
+
+            Console.ReadLine();
+        }
+
+
+        static string TersineCevir(string deger)
+        {
+            string sonuc = "";
+            for (int i = deger.Length - 1; i >= 0; i--)
+            {
+                sonuc += deger[i];
+            }
+            return sonuc;
+        }
+    }
+}
