9113a9597d90cc3d265f7fc0a1b3fb70495c5c97
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2022-03-06 19:29:32 +0300

    sdasad

---
+++ MvcWebUI/wwwroot/Html_Javascript_Css_Intro/04_BasicJavaScript.js
@@ -0,0 +1,33 @@
+﻿function ucgenDoldur(tersten = false) {
+    var sonuc;
+    // var sonucArray= new [] iki şekilde de array ekleyebiliyorsun.
+    var sonucArray = new Array();
+    for (var i = 0; i < 5; i++) {
+        sonuc = "";
+        for (var j = 0; j <= i; j++) {
+            sonuc += "*";
+        }
+        sonucArray[i] = sonuc;
+    }
+    sonuc = "";
+    if (!tersten) {
+
+        for (var i = 0; i < sonucArray.length; i++) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    }
+    else {
+        for (var i = sonucArray.length - 1; i >= 0; i--) {
+            sonuc += sonucArray[i] + "\n";
+        }
+    /*console.log(sonuc);*/
+    return sonuc;
+}
+function onLoadIleDoldur() {
+    var myTextArea = document.getElementById("mytextareaid");
+    myTextArea.value = ucgenDoldur();
+}
+function onClickDoldur() {
+    var myTextArea = document.getElementById("mytextareaid");
+    myTextArea.value = ucgenDoldur();
+}
