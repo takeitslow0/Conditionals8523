c08dcb8df90e109697f7484658a653b8c7b92ad3
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2022-03-13 17:44:36 +0300

    asasas

--- MvcWebUI/wwwroot/Html_Javascript_Css_Intro/04_Bilgisayar.js
+++ MvcWebUI/wwwroot/Html_Javascript_Css_Intro/04_Bilgisayar.js
@@ -3,28 +3,28 @@
     var model = document.getElementById("model");
     var hiz = document.getElementById("hiz");
     var hafiza = document.getElementById("hafiza");
-    var susogutma = document.getElementById("susogutma");
-    var uretimtarihi = document.getElementById("uretimtarihi");
+    var suSogutma = document.getElementById("susogutma");
+    var uretimTarihi = document.getElementById("uretimtarihi");
     let bilgisayar = new Bilgisayar(
         marka.value,
         model.value,
         parseFloat(hiz.value),
         parseInt(hafiza.value),
         susogutma.checked,
-        new Date(uretimtarihi.value + " 00:00:00")
+        new Date(uretimTarihi.value + " 00:00:00")
     );
     var p = document.getElementById("bilgisayar");
     p.innerHTML = bilgisayar.getir();
 }
 
 class Bilgisayar {
-    constructor(marka, model, hiz, hafiza, susogutma, uretimtarihi) {
+    constructor(marka, model, hiz, hafiza, suSogutma, uretimTarihi) {
         this.marka = marka;
         this.model = model;
         this.hiz = hiz;
         this.hafiza = hafiza;
-        this.susogutma = susogutma;
-        this.uretimtarihi = uretimtarihi;
+        this.suSogutma = suSogutma;
+        this.uretimTarihi = uretimTarihi;
     }
 
     getir() {
@@ -33,7 +33,7 @@ class Bilgisayar {
             '<b><i>Model:</i></b> ' + this.model + '<br />' +
             '<b><i>İşlemci Hızı:</i></b> ' + this.hiz + ' GHz<br />' +
             '<b><i>Hafıza:</i></b> ' + this.hafiza + ' GB<br />' +
-            '<b><i>Su Soğutma:</i></b> ' + (this.susogutma ? 'Evet' : 'Hayır') + '<br />' +
-            '<b><i>Üretim Tarihi:</i></b> ' + this.uretimtarihi.toLocalString('tr-TR');
+            '<b><i>Su Soğutma:</i></b> ' + (this.suSogutma ? 'Evet' : 'Hayır') + '<br />' +
+            '<b><i>Üretim Tarihi:</i></b> ' + this.uretimTarihi.toLocaleString('tr-TR');
     }
 }
---
+++ MvcWebUI/wwwroot/Html_Javascript_Css_Intro/05_BasicjQuery.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    console.log("script loaded");
+    var inputText;
+
+    $("#bSetText").click(function () {
+        inputText = $("#tbInput1").val(); // textbox ddl ve listbox için multiple değerler okunur.
+        $("#sOutput1").text(inputText); // span, label, p gibi şeyleri kullanıcıya göstermek için .text kullanılır.
+    });
+    $("#bsetHtml").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#sOutput2").html('<label style="color:blue;font-size:24px;"><b>' + inputText + '</b></label>');
+    });
+
+    $("#bSetValue").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#tbOutput1").val(inputText);
+    });
